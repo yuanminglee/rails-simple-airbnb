4e953fb175f1a0a660d38b9f756a2058c8c4abe0
YuanMing Lee <yuanming.lee91@gmail.com>
2021-02-20 12:49:00 +0800

    Seed data and create index view

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# Flats Controller
 class FlatsController < ApplicationController
+  def index
+    @flats = Flat.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+10.times do
+  Flat.create!(
+    name: Faker::Address.community,
+    address: Faker::Address.full_address,
+    description: Faker::TvShows::SiliconValley.quote,
+    price_per_night: rand(300),
+    number_of_guests: rand(10)
+  )
+end
