4a4506546623ac312feccad4e1240f964ba3b527
YuanMing Lee <yuanming.lee91@gmail.com>
2021-02-20 14:39:05 +0800

    Implement show action

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -2,7 +2,37 @@
 
 # Flats Controller
 class FlatsController < ApplicationController
+  before_action :set_flat, only: %i[show]
+
   def index
     @flats = Flat.all
+  end
+
+  def show
+    render layout: 'banner'
+  end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
   end
 end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,3 @@
 class Flat < ApplicationRecord
+  validates_presence_of :name, :address, :price_per_night, :number_of_guests
 end
